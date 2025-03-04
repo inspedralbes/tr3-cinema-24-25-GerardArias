633f417e9c38d861a61857901dfb08c34e36f876
Gerard Arias <a23gerarimar@inspedralbes.cat>
2025-03-04 12:33:34 +0100

    Migrations + install api

---
+++ back/laravel/database/migrations/2025_03_04_104904_create_sessions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('movie_id')->constrained('movies')->onDelete('cascade');
+            $table->date('date');
+            $table->time('time');
+            $table->boolean('vip_enabled');
+            $table->boolean('is_discount_day')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
---
+++ back/laravel/database/migrations/2025_03_04_104930_create_seats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('seats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('session_id')->constrained('sessions')->onDelete('cascade');
+            $table->char('row', 1);
+            $table->integer('number');
+            $table->enum('type', ['Normal', 'VIP']);
+            $table->enum('status', ['Disponible', 'Ocupada']);
+            $table->timestamps();
+            $table->unique(['session_id', 'row', 'number']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('seats');
+    }
+};
