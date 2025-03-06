ed718e4a3e49e43924bd9f13f05bccd98ce02621
GerardArias <a23gerarimar@inspedralbes.cat>
2025-03-06 13:12:36 +0100

    front empezado + normalizecss

--- front/app.vue
+++ front/app.vue
@@ -3,13 +3,18 @@
     <!-- Navbar -->
     <nav class="navbar">
       <div class="navbar-container">
-        <router-link to="/" class="navbar-item">Inicio</router-link>
-        <router-link to="/sesiones" class="navbar-item">Sesiones</router-link>
-        <router-link to="/peliculas" class="navbar-item">Películas</router-link>
-        <router-link to="/login" class="navbar-item">Login</router-link>
-        <router-link to="/carrito" class="navbar-item">
-          Carrito ({{ carritoItems }})
-        </router-link>
+        <div class="navbar-left">
+          <router-link to="/" class="navbar-item">Inicio</router-link>
+          <router-link to="/peliculas" class="navbar-item">Películas</router-link>
+          <router-link to="/sesiones" class="navbar-item">Sesiones</router-link>
+        </div>
+
+        <div class="navbar-right">
+          <router-link to="/carrito" class="navbar-item">
+            <i class="fa fa-shopping-cart"></i> ({{ carritoItems }})
+          </router-link>
+          <router-link to="/login" class="navbar-item">Login</router-link>
+        </div>
       </div>
     </nav>
 
@@ -42,23 +47,38 @@ export default {
 
 <style scoped>
 .navbar {
-  background-color: #333;
+  background-color: #173317;
   padding: 1rem;
   color: white;
 }
 
 .navbar-container {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between; /* Separamos los elementos entre izquierda y derecha */
+  align-items: center;
+}
+
+.navbar-left, .navbar-right {
+  display: flex;
+  align-items: center;
 }
 
 .navbar-item {
   color: white;
   text-decoration: none;
   font-size: 1.2rem;
+  margin-right: 20px; /* Espaciado entre los elementos */
 }
 
 .navbar-item:hover {
   text-decoration: underline;
+}
+
+.fa-shopping-cart {
+  margin-right: 8px; /* Espaciado entre el ícono y el texto */
+}
+
+.navbar-right {
+  justify-content: flex-end; /* Alinea el login y el carrito a la derecha */
 }
 </style>
---
+++ front/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <h1>Bienvenido al cine</h1>
+    </div>
+  </template>
+  
+  <script setup>
+  // Aquí va la lógica de tu componente
+  </script>
+  
+  <style scoped>
+  /* Tus estilos aquí */
+  </style>
+  
