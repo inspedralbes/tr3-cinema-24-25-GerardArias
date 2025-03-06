30817003f7e2d42effd349571123d3a55fd6aff2
Gerard Arias <a23gerarimar@inspedralbes.cat>
2025-03-06 12:56:09 +0100

    landing

--- front/app.vue
+++ front/app.vue
@@ -1,6 +1,64 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- Navbar -->
+    <nav class="navbar">
+      <div class="navbar-container">
+        <router-link to="/" class="navbar-item">Inicio</router-link>
+        <router-link to="/sesiones" class="navbar-item">Sesiones</router-link>
+        <router-link to="/peliculas" class="navbar-item">Películas</router-link>
+        <router-link to="/login" class="navbar-item">Login</router-link>
+        <router-link to="/carrito" class="navbar-item">
+          Carrito ({{ carritoItems }})
+        </router-link>
+      </div>
+    </nav>
+
+    <!-- Contenido Principal -->
+    <nuxt />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      carrito: []
+    };
+  },
+  computed: {
+    // Computed para mostrar el número de artículos en el carrito
+    carritoItems() {
+      return this.carrito.length;
+    }
+  },
+  methods: {
+    // Método para añadir productos al carrito (ejemplo)
+    agregarAlCarrito(producto) {
+      this.carrito.push(producto);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.navbar {
+  background-color: #333;
+  padding: 1rem;
+  color: white;
+}
+
+.navbar-container {
+  display: flex;
+  justify-content: space-around;
+}
+
+.navbar-item {
+  color: white;
+  text-decoration: none;
+  font-size: 1.2rem;
+}
+
+.navbar-item:hover {
+  text-decoration: underline;
+}
+</style>
