786b3aaeada9a09dc599af82b179c683bce3f5f4
GerardArias <a23gerarimar@inspedralbes.cat>
2025-03-12 11:37:57 +0100

    prova

--- back/laravel/app/Models/Seats.php
+++ back/laravel/app/Models/Seats.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,7 +9,11 @@ class Seats extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['session_id', 'row', 'number', 'type', 'status'];
+    protected $fillable = ['session_id', 'seats_json'];
+
+    protected $casts = [
+        'seats_json' => 'array',
+    ];
 
     public function session()
     {
