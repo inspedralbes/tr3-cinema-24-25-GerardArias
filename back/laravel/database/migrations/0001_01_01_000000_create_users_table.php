7ed663ed3ada21dee814c042ff90aab5b03e023e
Gerard Arias <a23gerarimar@inspedralbes.cat>
2025-03-13 10:30:45 +0100

    login arreglat

--- back/laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ back/laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,11 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('lastname')->nullable();
+            $table->string('lastname')->default('');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone')->nullable();
+            $table->string('phone')->default('');
             $table->rememberToken();
             $table->timestamps();
         });
