860952aba5f5c96c1dc2020cb36ed23ff52cff8f
GerardArias <a23gerarimar@inspedralbes.cat>
2025-03-17 10:49:36 +0100

    modelo actualitzatt

--- back/laravel/app/Models/Tickets.php
+++ back/laravel/app/Models/Tickets.php
@@ -9,15 +9,28 @@ class Tickets extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'session_id', 'seat_id', 'price'];
+    protected $table = 'tickets';
 
-    public function session()
+    protected $fillable = [
+        'email',
+        'session_id',
+        'seat_id',
+        'price',
+    ];
+
+    /**
+     * Relación con la sesión de película.
+     */
+    public function filmSession()
     {
-        return $this->belongsTo(FilmSessions::class);
+        return $this->belongsTo(FilmSessions::class, 'session_id');
     }
 
+    /**
+     * Relación con el asiento.
+     */
     public function seat()
     {
-        return $this->belongsTo(Seats::class);
+        return $this->belongsTo(Seats::class, 'seat_id');
     }
 }
