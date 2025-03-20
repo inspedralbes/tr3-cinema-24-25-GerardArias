020b785098034a5a7569780381ae435676475023
Gerard Arias <a23gerarimar@inspedralbes.cat>
2025-03-20 08:45:39 +0100

    a

--- back/laravel/app/Mail/TicketPurchased.php
+++ back/laravel/app/Mail/TicketPurchased.php
@@ -5,7 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use SimpleSoftwareIO\QrCode\Facades\QrCode; // Importar la librería para generar el QR
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Illuminate\Support\Facades\Storage;
 
 class TicketPurchased extends Mailable
@@ -21,22 +21,17 @@ class TicketPurchased extends Mailable
 
     public function build()
     {
-        // Crear el QR para cada ticket y guardarlo temporalmente
         foreach ($this->tickets as $ticket) {
-            // Generar el contenido del QR (puedes personalizarlo con la información del ticket)
             $qrCodeContent = 'Sesion: ' . $ticket->session_id . ' Asiento: ' . $ticket->seat_id;
             
-            // Generar el QR y guardarlo en una carpeta temporal
             $qrCodeImage = QrCode::format('png')->size(150)->generate($qrCodeContent);
             $fileName = 'ticket_qr_' . $ticket->id . '.png';
             $filePath = storage_path('app/public/' . $fileName);
             file_put_contents($filePath, $qrCodeImage);
 
-            // Agregar el archivo al ticket
             $ticket->qr_code_path = $filePath;
         }
 
-        // Enviar el correo con los tickets y los QR generados
         return $this->subject('Compra de Entradas')
             ->view('emails.ticket_purchased');
     }
