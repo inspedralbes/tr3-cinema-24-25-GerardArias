a8d8f0bbdeced0a9ffe2bd458e6595fa648e2a73
GerardArias <a23gerarimar@inspedralbes.cat>
2025-03-20 09:00:37 +0100

    arreglar enviament de correu

--- back/laravel/app/Mail/TicketPurchased.php
+++ back/laravel/app/Mail/TicketPurchased.php
@@ -5,8 +5,6 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
-use Illuminate\Support\Facades\Storage;
 
 class TicketPurchased extends Mailable
 {
@@ -21,18 +19,7 @@ class TicketPurchased extends Mailable
 
     public function build()
     {
-        foreach ($this->tickets as $ticket) {
-            $qrCodeContent = 'Sesion: ' . $ticket->session_id . ' Asiento: ' . $ticket->seat_id;
-            
-            $qrCodeImage = QrCode::format('png')->size(150)->generate($qrCodeContent);
-            $fileName = 'ticket_qr_' . $ticket->id . '.png';
-            $filePath = storage_path('app/public/' . $fileName);
-            file_put_contents($filePath, $qrCodeImage);
-
-            $ticket->qr_code_path = $filePath;
-        }
-
         return $this->subject('Compra de Entradas')
-            ->view('emails.ticket_purchased');
+            ->view('emails.ticket_purchased', ['tickets' => $this->tickets]);
     }
 }
